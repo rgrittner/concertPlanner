037f8e80d9010703cd5052de869882ecbf6ceb74
Renee Grittner <renee.grittner@gmail.com>
2018-02-11 01:35:27 -0600

    added hibernate annotations to Musician

--- src/main/java/entity/Musician.java
+++ src/main/java/entity/Musician.java
@@ -1,14 +1,46 @@
 package entity;
 
+import com.sun.tools.javah.Gen;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * A class to represent a musician.
+ * @author Renee Grittner
+ */
+@Entity(name = "Musician")
+@Table(name = "Musician")
 public class Musician {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private int id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
 
+    /**
+     * Instantiates a new Musician.
+     */
     public Musician() {
     }
 
+    /**
+     * Instantiates a new Musician.
+     *
+     * @param id          the id
+     * @param firstName   the first name
+     * @param lastName    the last name
+     * @param phoneNumber the phone number
+     */
     public Musician(int id, String firstName, String lastName, String phoneNumber) {
         this.id = id;
         this.firstName = firstName;
@@ -16,34 +48,74 @@ public class Musician {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Gets phone number.
+     *
+     * @return the phone number
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Sets phone number.
+     *
+     * @param phoneNumber the phone number
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
