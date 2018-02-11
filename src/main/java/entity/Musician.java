23e476504ea37f293d8fe97076e228eb5006ab00
Renee Grittner <renee.grittner@gmail.com>
2018-02-11 01:28:18 -0600

    added basics of Musician class

---
+++ src/main/java/entity/Musician.java
@@ -0,0 +1,58 @@
+package entity;
+
+public class Musician {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Musician() {
+    }
+
+    public Musician(int id, String firstName, String lastName, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString(){
+        return "Musician { " +
+                "first name = " + firstName + '\'' +
+                "last name = " + lastName + '\'' +
+                "phone = " + phoneNumber;
+    }
+}
