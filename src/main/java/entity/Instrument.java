d2b1754447408bc08cf2b5050d149571bed47114
Renee Grittner <renee.grittner@gmail.com>
2018-02-23 23:35:09 -0600

    saveOrUpdate working with equals
    Just commented out all the relational items with the instrument classes to get the tests working with equals in nationality class for now.

--- src/main/java/entity/Instrument.java
+++ src/main/java/entity/Instrument.java
@@ -21,8 +21,8 @@ public class Instrument {
     @Column(name = "name")
     private String name;
 
-    @ManyToOne
-    private InstrumentCategory instrumentCategory;
+//    @ManyToOne
+//    private InstrumentCategory instrumentCategory;
 
     /**
      * Instantiates a new Instrument.
@@ -35,12 +35,12 @@ public class Instrument {
      *
      * @param id                 the id
      * @param name               the name
-     * @param instrumentCategory the instrument category
+   //  * @param instrumentCategory the instrument category
      */
-    public Instrument(int id, String name, InstrumentCategory instrumentCategory) {
+    public Instrument(int id, String name) {
         this.id = id;
         this.name = name;
-        this.instrumentCategory = instrumentCategory;
+       // this.instrumentCategory = instrumentCategory;
     }
 
     /**
@@ -79,30 +79,30 @@ public class Instrument {
         this.name = name;
     }
 
-    /**
-     * Gets instrument category.
-     *
-     * @return the instrument category
-     */
-    public InstrumentCategory getInstrumentCategory() {
-        return instrumentCategory;
-    }
-
-    /**
-     * Sets instrument category.
-     *
-     * @param instrumentCategory the instrument category
-     */
-    public void setInstrumentCategory(InstrumentCategory instrumentCategory) {
-        this.instrumentCategory = instrumentCategory;
-    }
+//    /**
+//     * Gets instrument category.
+//     *
+//     * @return the instrument category
+//     */
+//    public InstrumentCategory getInstrumentCategory() {
+//        return instrumentCategory;
+//    }
+//
+//    /**
+//     * Sets instrument category.
+//     *
+//     * @param instrumentCategory the instrument category
+//     */
+//    public void setInstrumentCategory(InstrumentCategory instrumentCategory) {
+//        this.instrumentCategory = instrumentCategory;
+//    }
 
     @Override
     public String toString() {
         return "Instrument{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", instrumentCategory=" + instrumentCategory +
+                ", instrumentCategory=" +
                 '}';
     }
 }
