931306e262b7668772a8fcc4c13dd2cb944f4e1d
Renee Grittner <renee.grittner@gmail.com>
2018-02-24 03:34:38 -0600

    More composition dao

--- src/test/java/persistence/CompositionDaoTest.java
+++ src/test/java/persistence/CompositionDaoTest.java
@@ -1,4 +1,21 @@
 package persistence;
 
+import entity.Composition;
+import org.junit.jupiter.api.BeforeEach;
+import util.Database;
+
 public class CompositionDaoTest {
+    GenericDao genericDao;
+
+    /**
+     * Set up.
+     * Reset the Nationality table to a known state.
+     */
+    @BeforeEach
+    void setUp(){
+        Database database = Database.getInstance();
+        database.runSQL("cleanAll.sql");
+
+        genericDao = new GenericDao(Composition.class);
+    }
 }
