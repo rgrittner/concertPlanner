1123542567cbffcba56043d2485d49b3015a33a5
Renee Grittner <renee.grittner@gmail.com>
2018-02-24 01:53:50 -0600

    Added some annotations, tests still running ok

--- src/main/java/entity/InstrumentCategory.java
+++ src/main/java/entity/InstrumentCategory.java
@@ -1,4 +1,19 @@
 package entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity(name = "InstrumentCategory")
+@Table(name = "Instrument_Category")
 public class InstrumentCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int Id;
+    
+    private String category;
+
+
 }
