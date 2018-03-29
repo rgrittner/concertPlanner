4518967499b0c7e923da194eea5ac9918dca52db
Renee Grittner <renee.grittner@gmail.com>
2018-03-29 18:21:07 -0500

    More work

---
+++ src/main/java/com/reneegrittner/controller/AddInstrumentCategory.java
@@ -0,0 +1,45 @@
+package com.reneegrittner.controller;
+
+import com.reneegrittner.entity.InstrumentCategory;
+import com.reneegrittner.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        urlPatterns = {"/ensemble/addInstrumentCategory"}
+)
+public class AddInstrumentCategory extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/protected/addMusician.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        InstrumentCategory categoryToBeAdded = new InstrumentCategory();
+        categoryToBeAdded.setCategory(req.getParameter("category"));
+
+
+
+        GenericDao genericDao = new GenericDao(InstrumentCategory.class);
+
+
+        genericDao.insert(categoryToBeAdded);
+
+
+
+        String url = "/concertPlanner/ensemble/instruments";
+
+        resp.sendRedirect(url);
+
+        //TODO Data verifications
+    }
+}
