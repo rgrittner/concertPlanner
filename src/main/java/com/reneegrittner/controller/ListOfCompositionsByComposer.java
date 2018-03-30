2cb352abd7a11b402dab63ee2c5969c531e38390
Renee Grittner <renee.grittner@gmail.com>
2018-03-30 15:32:28 -0500

    List of compositions by composer servlet

---
+++ src/main/java/com/reneegrittner/controller/ListOfCompositionsByComposer.java
@@ -0,0 +1,39 @@
+package com.reneegrittner.controller;
+
+import com.reneegrittner.entity.Composer;
+import com.reneegrittner.entity.Composition;
+import com.reneegrittner.persistence.GenericDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        urlPatterns = {"/ensemble/compositionsByComposer"}
+)
+public class ListOfCompositionsByComposer extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String composerIdFromForm = req.getParameter("composerId");
+        Integer idFromParam = Integer.parseInt(req.getParameter("param"));
+
+        logger.debug("Composer Id: " + composerIdFromForm);
+        logger.debug("Composer Id from param: " + idFromParam);
+
+        GenericDao dao2 = new GenericDao<>(Composer.class);
+        Composer composer = (Composer) dao2.getById(idFromParam);
+
+        GenericDao dao = new GenericDao<>(Composition.class);
+        req.setAttribute("composerCompositions", dao.getByPropertyEqual("composerId", composer));
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/protected/compositionsOfComposer.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
