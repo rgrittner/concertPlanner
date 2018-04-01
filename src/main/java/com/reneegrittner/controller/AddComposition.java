50ea8924a35f12516506f3f6f6be9cff34c7051b
Renee Grittner <renee.grittner@gmail.com>
2018-04-01 18:49:42 -0500

    ready to test composition add

--- src/main/java/com/reneegrittner/controller/AddComposition.java
+++ src/main/java/com/reneegrittner/controller/AddComposition.java
@@ -44,9 +44,9 @@ public class AddComposition extends HttpServlet {
         String numberOfPlayersFromForm = req.getParameter("numberOfPlayers");
         String notesFromform = "notes";
 
-        // Convert String data to correct data type for variable
+        // Convert String data to correct data type for variable in entitiy
         Integer composerIdAsInteger = Integer.parseInt(composerIdFromForm);
-        Integer drationAsInteger = Integer.parseInt(durationFromForm);
+        Integer durationAsInteger = Integer.parseInt(durationFromForm);
         Integer yearAsInteger = Integer.parseInt(yearFromForm);
         Boolean clocksCommissionAsBool = Boolean.parseBoolean(clocksCommissionFromForm);
         Integer numberOfPlayersAsInteger = Integer.parseInt(numberOfPlayersFromForm);
@@ -55,22 +55,28 @@ public class AddComposition extends HttpServlet {
         GenericDao composerDao = new GenericDao<>(Composer.class);
         Composer composerToBeAdded = (Composer) composerDao.getById(composerIdAsInteger);
 
+        //Create new Composition object and set it's properties
+        Composition compositionToBeAdded = new Composition();
 
-        //
+        compositionToBeAdded.setYearComposed(yearAsInteger);
+        compositionToBeAdded.setNumberOfPlayers(numberOfPlayersAsInteger);
+        compositionToBeAdded.setDuration(durationAsInteger);
+        compositionToBeAdded.setComposer(composerToBeAdded);
+        compositionToBeAdded.setClocksCommission(clocksCommissionAsBool);
+        compositionToBeAdded.setTitle(titleFromForm);
 
+        //Check nullable fields for data
+        if (arrangerFromForm.length() > 0) {
+            compositionToBeAdded.setArranger(arrangerFromForm);
+        }
+        if (notesFromform.length() > 0){
+            compositionToBeAdded.setNotes(notesFromform);
+        }
 
+        //TODO Some sort of validation?
 
-
-
-
-        // Check for someone that already exists?
-        // If not found then add?
-
-        // Search by object? search by two parameters?
-
-        GenericDao genericDao = new GenericDao(Composition.class);
-
-        //genericDao.insert(compositionToBeAdded);
+        GenericDao genericDao = new GenericDao<>(Composition.class);
+        genericDao.insert(compositionToBeAdded);
 
 
         String url = "/protected/compositions";
