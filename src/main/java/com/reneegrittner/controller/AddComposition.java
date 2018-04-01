0c0dd1f937692dc068f68efebe5e4331bbec0dfa
Renee Grittner <renee.grittner@gmail.com>
2018-04-01 10:01:53 -0500

    Created addComposition doGet method

--- src/main/java/com/reneegrittner/controller/AddComposition.java
+++ src/main/java/com/reneegrittner/controller/AddComposition.java
@@ -1,4 +1,30 @@
 package com.reneegrittner.controller;
 
-public class AddComposition {
+import com.reneegrittner.entity.Composer;
+import com.reneegrittner.persistence.GenericDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        urlPatterns = {"/ensemble/addComposition"}
+)
+public class AddComposition extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        GenericDao dao = new GenericDao<>(Composer.class);
+        req.setAttribute("composers", dao.getAll());
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/protected/addComposition.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
