979eb8734ae613e916b94e41db10a3ff7fa46a9b
Renee Grittner <renee.grittner@gmail.com>
2018-04-01 22:21:35 -0500

    Getting things hooked up

--- src/main/java/com/reneegrittner/controller/AddInstrumentationStart.java
+++ src/main/java/com/reneegrittner/controller/AddInstrumentationStart.java
@@ -1,4 +1,26 @@
 package com.reneegrittner.controller;
 
-public class AddInstrumentationStart {
+import com.reneegrittner.entity.InstrumentCategory;
+import com.reneegrittner.persistence.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        urlPatterns = {"/ensemble/addPlayerInstrumentation"}
+)
+public class AddInstrumentationStart extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        GenericDao dao = new GenericDao(InstrumentCategory.class);
+        req.setAttribute("instrumentCat", dao.getAll());
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/concertPlanner/protected/addInstrumentationStart.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
--- src/main/webapp/protected/addInstrumentationStart.jsp
+++ src/main/webapp/protected/addInstrumentationStart.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-
+<h1>Made it!</h1>
 </body>
 </html>
