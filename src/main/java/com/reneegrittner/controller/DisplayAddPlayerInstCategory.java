7811a02cf323b3da33b54be54e1efc43ab71299e
Renee Grittner <renee.grittner@gmail.com>
2018-04-10 19:36:05 -0500

    genericising redirect

--- src/main/java/com/reneegrittner/controller/DisplayAddPlayerInstCategory.java
+++ src/main/java/com/reneegrittner/controller/DisplayAddPlayerInstCategory.java
@@ -1,6 +1,7 @@
 package com.reneegrittner.controller;
 
 import com.reneegrittner.entity.Composition;
+import com.reneegrittner.entity.CompositionInstrument;
 import com.reneegrittner.entity.Instrument;
 import com.reneegrittner.entity.InstrumentCategory;
 import com.reneegrittner.persistence.GenericDao;
@@ -29,6 +30,7 @@ public class DisplayAddPlayerInstCategory extends HttpServlet {
         String categoryFromForm = req.getParameter("category");
         Integer compositionIdFromForm = Integer.parseInt(req.getParameter("compositionId"));
         int playerNumber = Integer.parseInt(req.getParameter("playerNumber"));
+        Integer categoryIdFromForm = Integer.parseInt(req.getParameter("categoryId"));
 
         // Get the composition information to send along to jsp
         GenericDao<Composition> compositionDao = new GenericDao<>(Composition.class);
@@ -40,54 +42,66 @@ public class DisplayAddPlayerInstCategory extends HttpServlet {
         List<InstrumentCategory> category = categoryDao.getByPropertyEqual("category", categoryFromForm);
         int categoryId = category.get(0).getId();
         req.setAttribute("instruments", dao.getByPropertyEqual("instrumentCategory", categoryId));
-        logger.info("This is what comes back: " + dao.getByPropertyEqual("instrumentCategory", categoryId));
 
         // Send the playerNumber as well
         req.setAttribute("playerNumber", playerNumber);
+        req.setAttribute("categoryId", categoryIdFromForm);
 
 
         // Redirect depending on which type... may be able to reduce this to a single page.
-        String url = "";
-        switch (categoryFromForm) {
-            case "Keyboards": url = "/protected/addKeyboards.jsp";
-                    break;
-            case "Skins": url = "/protected/addSkins.jsp";
-                    break;
-            case "Woods": url = "/protected/addWoods.jsp";
-                    break;
-            case "Metals": url = "/protected/addMetals.jsp";
-                    break;
-            case "Other": url = "/protected/addOther.jsp";
-                    break;
-            case "Timpani": url = "/protected/addTimpani.jsp";
-                    break;
-            case "Accessory": url = "/protected/addAccessory.jsp";
-                    break;
-        }
+//        String url = "";
+//        switch (categoryFromForm) {
+//            case "Keyboards": url = "/protected/addKeyboards.jsp";
+//                    break;
+//            case "Skins": url = "/protected/addSkins.jsp";
+//                    break;
+//            case "Woods": url = "/protected/addWoods.jsp";
+//                    break;
+//            case "Metals": url = "/protected/addMetals.jsp";
+//                    break;
+//            case "Other": url = "/protected/addOther.jsp";
+//                    break;
+//            case "Timpani": url = "/protected/addTimpani.jsp";
+//                    break;
+//            case "Accessory": url = "/protected/addAccessory.jsp";
+//                    break;
+//        }
 
-        RequestDispatcher dispatcher = req.getRequestDispatcher(url);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/protected/addAccessory.jsp");
         dispatcher.forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // get category id
+        Integer categoryId = Integer.parseInt(req.getParameter("categoryId"));
         // get player number
+        Integer playerNumber = Integer.parseInt(req.getParameter("playerNumber"));
 
         // get composition id
+        Integer compositionId = Integer.parseInt(req.getParameter("idOfComposition"));
 
+        // get composition object
+        GenericDao<Composition> compositionGenericDao = new GenericDao<>(Composition.class);
+        Composition composition = compositionGenericDao.getById(compositionId);
 
+        GenericDao<CompositionInstrument> compositionInstrumentGenericDao = new GenericDao<>(CompositionInstrument.class);
         // Find out how many instruments there currently are in current category
-        int numberOfInstrumentsOfCategory = dao.getByPropertyEqual("category", 7).size();
-        int current = 0;
-        while(current <= numberOfInstrumentsOfCategory){
-            int idOfCurrentInstrument = Integer.parseInt(req.getParameter("current"));
-            //check if this instrument exists for this player for this composition
-                // if yes
-                    // saveOrUpdate
-                // if no
-                    // insert
-
+       // GenericDao<InstrumentCategory> categoryGenericDao = new GenericDao<>(InstrumentCategory.class);
+        List<Instrument> listOfInstrumentsOfCategory = dao.getByPropertyEqual("instrumentCategory", categoryId);
+        for(Instrument currentInstrument: listOfInstrumentsOfCategory) {
+            Integer currentId = currentInstrument.getId();
+            // get quantity from form
+            CompositionInstrument compositionInstrument = new CompositionInstrument();
+            compositionInstrument.setInstrument(currentInstrument);
+            compositionInstrument.setPlayerNumber(playerNumber);
+            compositionInstrument.setComposition(composition);
+            compositionInstrument.setInstrumentQuantity(Integer.parseInt(req.getParameter("instrumentId" + currentId)));
+            compositionInstrumentGenericDao.insert(compositionInstrument);
         }
+
+        String url = "/concertPlanner/ensemble/addPlayerInstrumentation?player="+playerNumber+"&compositionId="+compositionId;
+        resp.sendRedirect(url);
 
     }
 }
