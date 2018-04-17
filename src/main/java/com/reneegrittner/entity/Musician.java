3b06a204fe934169b0597b2ecc8bfcad0a8379fd
Renee Grittner <renee.grittner@gmail.com>
2018-04-17 18:55:53 -0500

    updating cleanALl

--- src/main/java/com/reneegrittner/entity/Musician.java
+++ src/main/java/com/reneegrittner/entity/Musician.java
@@ -3,10 +3,12 @@ package com.reneegrittner.entity;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
 /**
  * A class to represent a musician.
+ *
  * @author Renee Grittner
  */
 @Entity(name = "Musician")
@@ -33,8 +35,13 @@ public class Musician {
     @Column(name = "status")
     private String status;
 
+<<<<<<< HEAD
     @Column(name = "user_Id")
     private int userId;
+=======
+    @OneToMany(mappedBy = "musician", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<ProgramComposition> listOfProgramComposition;
+>>>>>>> master
 
     /**
      * Instantiates a new Musician.
@@ -48,6 +55,8 @@ public class Musician {
      * @param firstName   the first name
      * @param lastName    the last name
      * @param phoneNumber the phone number
+     * @param email       the email
+     * @param status      the status
      */
     public Musician(String firstName, String lastName, String phoneNumber, String email, String status) {
 
@@ -159,6 +168,24 @@ public class Musician {
      * @param status the status
      */
     public void setStatus(String status) { this.status = status; }
+
+    /**
+     * Gets list of program composition.
+     *
+     * @return the list of program composition
+     */
+    public List<ProgramComposition> getListOfProgramComposition() {
+        return listOfProgramComposition;
+    }
+
+    /**
+     * Sets list of program composition.
+     *
+     * @param listOfProgramComposition the list of program composition
+     */
+    public void setListOfProgramComposition(List<ProgramComposition> listOfProgramComposition) {
+        this.listOfProgramComposition = listOfProgramComposition;
+    }
 
     @Override
     public String toString(){
