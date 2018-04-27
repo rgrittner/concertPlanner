17532f45fbbcea291ce95177307e031727f3a60e
Renee Grittner <renee.grittner@gmail.com>
2018-04-26 19:25:17 -0500

    Starting testable servlet logic

---
+++ src/main/java/com/reneegrittner/controllerLogic/ComposerLogic.java
@@ -0,0 +1,12 @@
+package com.reneegrittner.controllerLogic;
+
+public class ComposerLogic {
+
+    private int addComposer(){
+        int idOfComposerAdded = 1;
+
+
+
+        return idOfComposerAdded;
+    }
+}
