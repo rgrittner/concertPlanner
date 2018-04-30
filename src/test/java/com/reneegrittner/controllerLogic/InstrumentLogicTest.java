6204987cd9ae0b1609a5ecca2c4438f147471584
Renee Grittner <renee.grittner@gmail.com>
2018-04-30 15:10:53 -0500

    new tests for instrument category

--- src/test/java/com/reneegrittner/controllerLogic/InstrumentLogicTest.java
+++ src/test/java/com/reneegrittner/controllerLogic/InstrumentLogicTest.java
@@ -3,6 +3,8 @@ package com.reneegrittner.controllerLogic;
 import com.reneegrittner.entity.Instrument;
 import com.reneegrittner.persistence.GenericDao;
 import com.reneegrittner.util.DatabaseTwo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class InstrumentLogicTest {
     private GenericDao<Instrument> instrumentGenericDao = new GenericDao<>(Instrument.class);
     private InstrumentLogic instrumentLogic = new InstrumentLogic();
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
     @BeforeEach
     void setUp() {
@@ -30,5 +33,24 @@ class InstrumentLogicTest {
         Instrument instrument = instrumentLogic.createOrUpdateInstrument("test Instrument", "2", 1, "1");
         assertEquals(2, instrument.getInstrumentCategory().getId());
         assertEquals("test Instrument", instrument.getName());
+    }
+
+    @Test
+    void checkIfCategoryAlreadyExists() {
+        boolean okToInsert;
+        okToInsert = instrumentLogic.checkIfCategoryAlreadyExists("Keyboards", 1);
+        assertFalse(okToInsert);
+
+        okToInsert = instrumentLogic.checkIfCategoryAlreadyExists("wertty", 1);
+        assertTrue(okToInsert);
+
+        okToInsert = instrumentLogic.checkIfCategoryAlreadyExists("Keyboards", 100);
+        assertTrue(okToInsert);
+    }
+
+    @Test
+    void addNewInstrumentCategory() {
+        int id = instrumentLogic.addNewInstrumentCategory("Fruit", 100);
+        assertEquals(21, id);
     }
 }
