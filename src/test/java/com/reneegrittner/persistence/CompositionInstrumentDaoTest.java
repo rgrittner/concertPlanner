83aadab9c00e3ef9dcaebee6a0b71c8ae4607479
Renee Grittner <renee.grittner@gmail.com>
2018-04-29 19:55:03 -0500

    test that instrumnt and composition are not deleted

--- src/test/java/com/reneegrittner/persistence/CompositionInstrumentDaoTest.java
+++ src/test/java/com/reneegrittner/persistence/CompositionInstrumentDaoTest.java
@@ -1,6 +1,8 @@
 package com.reneegrittner.persistence;
 
+import com.reneegrittner.entity.Composition;
 import com.reneegrittner.entity.CompositionInstrument;
+import com.reneegrittner.entity.Instrument;
 import com.reneegrittner.util.DatabaseTwo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,9 +12,12 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class CompositionInstrumentDaoTest {
     private GenericDao<CompositionInstrument> genericDao;
+    private GenericDao<Composition> compositionGenericDao = new GenericDao<>(Composition.class);
+    private GenericDao<Instrument> instrumentGenericDao = new GenericDao<>(Instrument.class);
     private final Logger logger = LogManager.getLogger(this.getClass());
 
     /**
@@ -54,6 +59,24 @@ public class CompositionInstrumentDaoTest {
        List<CompositionInstrument> instruments = genericDao.getByPropertyEqualCompositionInstrument(1, 1, 1);
        assertEquals(1, instruments.size());
 
+    }
+
+    @Test
+    void deleteInstrumentFromLinkingTableDoesNotDeleteCompositionOrInstrument(){
+        Instrument instrument =  instrumentGenericDao.getById(2);
+        Composition composition = compositionGenericDao.getById(1);
+
+        CompositionInstrument newInstrumentForComposition = new CompositionInstrument(2, 1, instrument, composition, 1);
+
+        int id = genericDao.insert(newInstrumentForComposition);
+        assertEquals(5, id);
+
+        genericDao.delete(newInstrumentForComposition);
+
+        assertNull(genericDao.getById(id));
+
+        assertEquals(instrument, instrumentGenericDao.getById(2));
+        assertEquals(composition, compositionGenericDao.getById(1));
     }
 
 
